e45f14290bc9547d8b808ca734042c39120cad38
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-10-16 08:17:06 -0700

    strdup

--- string_func.c
+++ string_func.c
@@ -30,3 +30,33 @@ return (0);
 }
 return (1);
 }
+
+/**
+ *_strdup - duplicate a string
+ *@c: input pointer
+ *Return: pointer
+ */
+
+char *_strdup(char *c)
+{
+int c_size;
+static char *dup;
+char *temp;
+if (c == NULL)
+return((char *)NULL);
+
+c_size = _strlen(c);
+dup = (char *)malloc(sizeof(char)*c_size+1);
+if (dup == NULL)
+return((char *)NULL);
+
+temp= dup;
+while (*c)
+{
+*temp = *c;
+temp++;
+c++;
+}
+*temp = '\0';
+return(dup);
+}
