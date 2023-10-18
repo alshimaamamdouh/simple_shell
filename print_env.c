ac06661c0639781b0a5790f4deeba66912d0c6ff
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-10-18 07:05:33 -0700

    update

--- print_env.c
+++ print_env.c
@@ -7,28 +7,18 @@
 
 void print_env(void)
 {
-char *env_info;
-int env_offset, i;
+int i;
 extern char **environ;
-char *c[7] = {"HOSTNAME", "LANGUAGE", "SHLVL",
-
-	"HOME", "HOSTNAME", "TERM", "PATH"};
-char *c_p[7] = {"USER=", "LANGUAGE=", "SHLVL=",
-
-	"HOME=", "LOGNAME=", "TERM=", "PATH="};
-
-for (i = 0; i < 7; i++)
+for (i = 0 ; environ[i] != 0; i++)
 {
-env_info = _getenv(c[i], environ, &env_offset);
-if (env_info)
+if (environ[i])
 {
-write(1, c_p[i], _strlen(c_p[i]));
-write(1, env_info, _strlen(env_info));
+write(1, environ[i], _strlen(environ[i]));
 write(1, "\n", _strlen("\n"));
 }
 else
 {
-write(1, "", _strlen(""));
+write(1, "\n", _strlen("\n"));
 }
 }
 }
